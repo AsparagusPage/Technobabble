ad5aae5356994e19be47d4457866618b02a53265
Asparagus <AsparagusPage@users.noreply.github.com>
2017-01-04 13:38:28 -0500

    Wrote function to clean text, grouped subtitles together by file.

--- dump-subtitles.py
+++ dump-subtitles.py
@@ -11,14 +11,26 @@ def main():
 
     # For each argument passed to this script (paths)
     for filename in sys.argv[1:]:
-        # For each sub-title object in that file
+        # text of subtitles in that file
+        scriptSubs = []
+        # For each sub-title object in the file
         for sub in pysrt.open(filename):
-            # Add a clean version of our text to our subtitle collection
-            subtitles.append(clean(sub.text))
+            # Add a clean version of our text to the script subtitle collection
+            scriptSubs.append(clean(sub.text))
+        # Add the clean version to the general subtitle collection
+        subtitles.append(" ".join(scriptSubs))
 
     save(subtitles)
 
 def clean(text):
+    # Remove "- " 
+    text = re.sub( "(-\s)+", "", text )
+    # Remove HTML
+    text = re.sub( "<.*>", "", text )
+    # Remove ellipses 
+    text = re.sub( "(\.\.\.)", "", text )
+    # Remove line breaks
+    text = re.sub( "\n", " ", text )
     return text
 
 def save(subs):
