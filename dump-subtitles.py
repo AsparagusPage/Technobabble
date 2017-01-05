f42d987f52f400b9700291849e39c5b5978c3f90
Asparagus <AsparagusPage@users.noreply.github.com>
2017-01-05 17:12:28 -0500

    Refactored and added to episode extraction.

--- dump-subtitles.py
+++ dump-subtitles.py
@@ -6,21 +6,25 @@ import re
 import pysrt
 
 def main():
+
+    for filename in sys.argv[1:]:
+        print(make_episode(filename))
+
+def get_subtitles():
     # text of subtitles
     subtitles = []
-
     # For each argument passed to this script (paths)
     for filename in sys.argv[1:]:
         # text of subtitles in that file
         scriptSubs = []
         # For each sub-title object in the file
-        for sub in pysrt.open(filename):
-            # Add a clean version of our text to the script subtitle collection
+        for sub in pysrt.open(filename, encoding="ISO-8859-2"):
+        #    # Add a clean version of our text to the script subtitle collection
             scriptSubs.append(clean(sub.text))
         # Add the clean version to the general subtitle collection
         subtitles.append(" ".join(scriptSubs))
 
-    save(subtitles)
+    return subtitles
 
 def clean(text):
     # Remove leading "- " and join lines
@@ -33,6 +37,12 @@ def clean(text):
 def save(subs):
     for text in subs:
         print(text)
+
+def make_episode(filename):
+    nums = re.search("(\d{1,2})x(\d{2})", filename)
+    episode = "S%sE%s" % (nums.group(1), nums.group(2))
+
+    return episode
 
 if __name__ == "__main__":
     main()
