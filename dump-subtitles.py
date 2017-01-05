769aa4f09456ee1b1a5ed003db65dbb2eca29ba2
Hackpot <chao-mu@hackpoetic.com>
2017-01-05 17:26:14 -0500

    Add argument parsing

--- dump-subtitles.py
+++ dump-subtitles.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
-import sys
+import argparse
 import re
+import sys
 
 import pysrt
 
 def main():
+    parser = argparse.ArgumentParser(description="Extract subtitles")
+    parser.add_argument("--encoding", default="ISO-8859-2", help="character encoding of subtitles")
+    parser.add_argument("subtitles", nargs="*", help="list of subtitle file names")
+    args = parser.parse_args()
 
-    for filename in sys.argv[1:]:
+    # subs = get_subtitles(args.encoding)
+    for filename in args.subtitles:
         print(make_episode(filename))
 
-def get_subtitles():
+def get_subtitles(encoding):
     # text of subtitles
     subtitles = []
     # For each argument passed to this script (paths)
@@ -18,7 +24,7 @@ def get_subtitles():
         # text of subtitles in that file
         scriptSubs = []
         # For each sub-title object in the file
-        for sub in pysrt.open(filename, encoding="ISO-8859-2"):
+        for sub in pysrt.open(filename, encoding=encoding):
         #    # Add a clean version of our text to the script subtitle collection
             scriptSubs.append(clean(sub.text))
         # Add the clean version to the general subtitle collection
